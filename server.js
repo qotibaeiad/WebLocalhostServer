632637915416bae229b5e2f45e077252db386efd
user <user@users-MacBook-Pro.local>
2024-03-04 15:33:19 +0200

    allow client

--- server.js
+++ server.js
@@ -7,13 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Explicitly set allowed origins
-//app.use(cors({ origin: 'https://newsweb-phi.vercel.app'}));
-app.use(cors({ origin: 'https://deplowwebhw2.vercel.app'}));
-app.use(cors({ origin: 'http://localhost:5173'}));
-
-
-
-
+const allowedOrigins = ['https://deplowwebhw2.vercel.app', 'http://localhost:5173'];
+app.use(cors({ origin: allowedOrigins }));
 
 app.use(express.json());
 
@@ -22,7 +17,6 @@ app.use(express.static('public'));
 
 // Connect to MongoDB
 const mongoDB = new MongoDB('mongodb+srv://qotibaeiad98:hrqk7o7dHydnV49a@newtailwind.wce8sqn.mongodb.net/tailwindweb', 'tailwindweb');
-
 
 (async () => {
   try {
@@ -67,54 +61,15 @@ const mongoDB = new MongoDB('mongodb+srv://qotibaeiad98:hrqk7o7dHydnV49a@newtail
 
   app.post('/api/updateUserData', async (req, res) => {
     try {
-        await dataHandler.updateuserdata(mongoDB)(req, res);
+      await dataHandler.updateuserdata(mongoDB)(req, res);
     } catch (error) {
-        console.error('Error updating user data:', error);
-        res.status(500).json({ success: false, message: 'Internal server error' });
+      console.error('Error updating user data:', error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
     }
-});
-  
+  });
 })();
 
 // Start the server
 app.listen(port, () => {
   console.log(`Deployed URL: ${process.env.DEPLOY_URL}`);
 });
-
-
-
-// async function addArticle(username,title, content, author, category, url) {
-//   const apiUrl = serverUrl + '/api/add-article';
-
-//   const articleData = {
-//     username,
-//     title,
-//     content,
-//     author,
-//     category,
-//     url,
-//   };
-//   try {
-//     const response = await fetch(apiUrl, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         // You may include additional headers like authentication tokens if required
-//       },
-//       body: JSON.stringify(articleData),
-//     });
-
-//     const data = await response.json();
-
-//     if (data.success) {
-//       console.log('Article added successfully');
-//       return true;
-//     } else {
-//       console.log('Failed to add article');
-//       return false;
-//     }
-//   } catch (error) {
-//     console.error('Error during article addition request:', error);
-//     return false;
-//   }
-// }
